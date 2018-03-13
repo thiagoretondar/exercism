0f7afbc3d02dcea04cb504d00676e574f7f5d53a
Thiago Retondar <t.retondar@gmail.com>
2018-03-12 23:31:39 -0300

    pangram solved

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,4 @@
 def is_pangram(sentence):
-    pass
+    all_alphabet_letters = set([chr(char) for char in range(ord('a'), ord('z'))])
+    all_letters_in_sentence = set(sentence.lower())
+    return all_alphabet_letters.issubset(all_letters_in_sentence)
